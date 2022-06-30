5f6fd6bfb8bae458a54c8deb864229757dba4c64
diegomduquec <diegomduquec@gmail.com>
2022-06-30 08:20:08 -0500

    Servidor corriendo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log("Señor, date prisa en socorrerme");
+
+const app = require('./app')
+const port = process.env.PORT || 3001
+app.listen(port,() => {
+    console.log(`server is runnig in port ${port}`);
+})
