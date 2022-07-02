f9615d60e25c0ed187eb55fd389c6cb68d5a4929
diegomduquec <diegomduquec@gmail.com>
2022-07-02 15:13:59 -0500

    EJS funcional, Empiezo: CSS - Login

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
+const path = require('path');
+
+//Definiendo carpeta pública
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Configurar el uso del motor de platillas EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Mostrando index EJS...
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+//Eliminar después
 console.log("Señor, date prisa en socorrerme");
 
 const app = require('./app')
+
+//Levandando el puerto. Configuración para adaptar el puerto en servidor externo
 const port = process.env.PORT || 3001
 app.listen(port,() => {
     console.log(`server is runnig in port ${port}`);
